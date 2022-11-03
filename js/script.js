435cc468619c482d5b30ffbabde356bf7b482af3
Marta Santos <mdpesa@gmail.com>
2022-11-03 18:40:31 +0100

    add code for script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = `papier`;
+
+printMessage( 'Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz');
