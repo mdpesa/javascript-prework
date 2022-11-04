ed581b935c2be5f544e81c8994ec0f69595945ae
Marta Santos <mdpesa@gmail.com>
2022-11-05 00:30:38 +0100

    add possibilities for computer moves

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+	computerMove = 'kamień';
+} else if (randomNumber == 2) {
+	computerMove = 'papier';
+} else if (randomNumber == 3) {
+	computerMove = 'nożyce';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = `papier`;
-
-printMessage( 'Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz');
+printMessage('Mój ruch to: ' + computerMove);
