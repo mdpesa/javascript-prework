67f1ca91f4529d0f65cd8ad9db1d86e252ab3df8
Marta Santos <mdpesa@gmail.com>
2022-11-09 16:35:44 +0100

    functions modification

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-function playGame(playerInput) {
+{
+const playGame=function(playerInput) {
 	clearMessages();
 
-	function getMoveName(argMoveId) {
+	const getMoveName=function(argMoveId) {
 		if (argMoveId == 1) {
 			return 'kamień';
 		} else if (argMoveId == 2) {
@@ -20,28 +21,11 @@ function playGame(playerInput) {
 
 	let argComputerMove = getMoveName(randomNumber);
 
-	/*if (randomNumber == 1) {
-        computerMove = 'kamień';
-    } else if (randomNumber == 2) {
-        computerMove = 'papier';
-    } else if (randomNumber == 3) {
-        computerMove = 'nożyce';
-    }*/
 	printMessage('Mój ruch to: ' + argComputerMove);
-
-	/*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
 
 	console.log('Gracz wpisał: ' + playerInput);
 
 	let argPlayerMove = getMoveName(playerInput);
-
-	/*if (playerInput == '1') {
-        playerMove = 'kamień';
-    } else if (playerInput == '2') {
-        playerMove = 'papier';
-    } else if (playerInput == '3') {
-        playerMove = 'nożyce';
-    }*/
 
 	printMessage('Twój ruch to: ' + argPlayerMove);
 
@@ -60,23 +44,6 @@ function playGame(playerInput) {
 		}
 	}
 	displayResult(argComputerMove, argPlayerMove);
-	/*if (computerMove == playerMove) {
-        printMessage('Mamy remis!');
-    } else if (computerMove == 'kamień' && playerMove == 'papier') {
-        printMessage('Ty wygrywasz!');
-    } else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-        printMessage('Ja wygrywam!');
-    } else if (computerMove == 'papier' && playerMove == 'kamień') {
-        printMessage('Ja wygrywam!');
-    } else if (computerMove == 'papier' && playerMove == 'nożyce') {
-        printMessage('Ty wygrywasz!');
-    } else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-        printMessage('Ty wygrywasz!');
-    } else if (computerMove == 'nożyce' && playerMove == 'papier') {
-        printMessage('Ja wygrywam!');
-    } else if (playerMove == 'nieznany ruch') {
-        printMessage('Wybierz 1, 2 lub 3!');
-    }*/
 }
 
 document.getElementById('play-rock').addEventListener('click', function () {
@@ -88,3 +55,5 @@ document.getElementById('play-paper').addEventListener('click', function () {
 document.getElementById('play-scissors').addEventListener('click', function () {
 	playGame('3');
 });
+
+}
